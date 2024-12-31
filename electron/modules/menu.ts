dd75e3f2cfd01a960dc6d2028fdbd70399741fea
tahsin4466 <tahsin4466@g.ucla.edu>
2025-01-01 02:09:43 +1100

    Extended Menu Scaffolding

--- electron/modules/menu.ts
+++ electron/modules/menu.ts
@@ -8,51 +8,91 @@ export function setupMenu(mainWindow: BrowserWindow) {
             label: "File",
             submenu: [
                 {
-                    label: "Open",
+                    label: "Open...",
+                    accelerator: isMac ? "Cmd+O" : "Ctrl+O", // Add shortcut for Open
                     click: () => {
                         mainWindow.webContents.send("menu-open-file");
                     },
                 },
                 {
                     label: "Save",
+                    accelerator: isMac ? "Cmd+S" : "Ctrl+S", // Add shortcut for Save
                     click: () => {
                         mainWindow.webContents.send("menu-save-file");
                     },
                 },
-                { type: "separator" },
+                {label: "Compile"}
             ],
         },
         {
             label: "Edit",
             submenu: [
-                { role: "undo" },
-                { role: "redo" },
+                { role: "undo", accelerator: isMac ? "Cmd+Z" : "Ctrl+Z" },
+                { role: "redo", accelerator: isMac ? "Shift+Cmd+Z" : "Ctrl+Y" },
                 { type: "separator" },
-                { role: "cut" },
-                { role: "copy" },
-                { role: "paste" },
+                { role: "cut", accelerator: isMac ? "Cmd+X" : "Ctrl+X" },
+                { role: "copy", accelerator: isMac ? "Cmd+C" : "Ctrl+C" },
+                { role: "paste", accelerator: isMac ? "Cmd+V" : "Ctrl+V" },
                 { type: "separator" },
-                { role: "selectAll" },
+                { role: "selectAll", accelerator: isMac ? "Cmd+A" : "Ctrl+A" },
             ],
         },
         {
             label: "View",
             submenu: [
-                { role: "reload" },
-                { role: "toggleDevTools" },
+                { role: "reload", accelerator: isMac ? "Cmd+R" : "Ctrl+R" },
+                { role: "toggleDevTools", accelerator: isMac ? "Alt+Cmd+I" : "Ctrl+Shift+I" },
                 { type: "separator" },
-                { role: "resetZoom" },
-                { role: "zoomIn" },
-                { role: "zoomOut" },
+                { role: "resetZoom", accelerator: "CmdOrCtrl+0" },
+                { role: "zoomIn", accelerator: "CmdOrCtrl+Plus" },
+                { role: "zoomOut", accelerator: "CmdOrCtrl+-" },
                 { type: "separator" },
-                { role: "togglefullscreen" },
+                { role: "togglefullscreen", accelerator: isMac ? "Ctrl+Cmd+F" : "F11" },
             ],
+        },
+        {
+            label: "Terminal",
+            submenu: [
+                { label: "Toggle Terminal" },
+                { label: "Clear Terminal" },
+                { type: "separator" },
+                { label: "Invoke Compiler from CLI "},
+                { label: "Invoke Runtime from CLI "}
+            ]
+
+        },
+        {
+            label: "Breakpoint",
+            submenu: [
+                { label: "Create Breakpoint" },
+                { label: "Remove Breakpoint" },
+            ]
+        },
+        {
+            label: "Execute",
+            submenu: [
+                { label: "Run File" },
+                { type: "separator" },
+                { label: "Start Debugger" },
+                { label: "Step Forward" },
+                { label: "Execute Remaining" },
+                { label: "Restart" },
+            ]
+        },
+        {
+            label: "VM",
+            submenu: [
+                { label: "Open VM View" },
+                { type: "separator" },
+                { label: "Dump to Terminal" },
+                { label: "Dump Parameters" },
+            ]
         },
         {
             label: "Window",
             role: "window",
             submenu: [
-                { role: "minimize" },
+                { role: "minimize", accelerator: isMac ? "Cmd+M" : "Alt+Space" },
                 { role: "zoom" },
             ],
         },
